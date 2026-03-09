bcfcfcc974840f6f9b30bf5f33d394857e4e24ca
Chris Brilianto Chandra <rejuvenation@Chriss-MacBook-Pro.local>
2026-03-10 04:11:45 +0800

    Added Scanner and balancing framework

--- Question1/Main.java
+++ Question1/Main.java
@@ -1,25 +1,16 @@
 package Question1;
 
+import java.util.Scanner;
+
 public class Main {
     String input;
     public static void main(String[] args) {
-        String input;
-        System.out.print("Enter a string : ");
-        input = System.console().readLine();
-        System.out.println("Checking if the string is balanced...");
-        is_Balanced(input);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string to check if it is balanced or not: ");
+        String inputString = sc.nextLine();
+        is_balanced(inputString);
     }
-    public static void is_Balanced(String str) {
-        String notBalanced = "The string is not balanced.";
-        String isPerfectlyBalanced = "The string is perfectly balanced.";
-        int inputLength = str.length();
-        if (inputLength % 2 != 0) {
-            System.out.println(notBalanced);
-            return;
-        }
-        System.out.println(isPerfectlyBalanced);
-        return;
+    public static void is_balanced(String inputString) {
         
     }
-
 } 
