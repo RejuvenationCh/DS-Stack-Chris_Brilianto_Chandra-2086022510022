a57da79309ccf73d28858ede3f936286dfb5beb3
Chris Brilianto Chandra <rejuvenation@Chriss-MacBook-Pro.local>
2026-03-10 04:23:37 +0800

    Made the main framework, still not working

--- Question1/Main.java
+++ Question1/Main.java
@@ -1,6 +1,7 @@
 package Question1;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Main {
     String input;
@@ -11,6 +12,45 @@ public class Main {
         is_balanced(inputString);
     }
     public static void is_balanced(String inputString) {
+        boolean isBalanced = false;
+        for (int i = 0; i < inputString.length(); i++) {
+            if (i % 2 != 0) {
+                break;
+            }
+            else {
+                char[] brackets = inputString.toCharArray();
+                Stack<Character> inputStack = new Stack<>();
+
+                for (char bracket : brackets) {
+                    if (bracket == '(' || bracket == '{' || bracket == '[') {
+                        inputStack.push(bracket);
+                    }
+                    else if (bracket == ')' && !inputStack.isEmpty() && inputStack.peek() == '(') {
+                        inputStack.pop();
+                    }
+                    else if (bracket == '}' && !inputStack.isEmpty() && inputStack.peek() == '{') {
+                        inputStack.pop();
+                    }
+                    else if (bracket == ']' && !inputStack.isEmpty() && inputStack.peek() == '[') {
+                        inputStack.pop();
+                    }
+                    else {
+                        isBalanced = false;
+                        break;
+                    }
+    
+                }
+            }
+
+                
+            }
+
+          if (isBalanced) {
+            System.out.println("The string is balanced.");
+        } else {
+            System.out.println("The string is not balanced.");
+        }
         
-    }
+
 } 
+}
