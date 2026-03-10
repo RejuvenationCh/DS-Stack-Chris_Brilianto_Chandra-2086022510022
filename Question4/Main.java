a2dbb97e235b8043c18269e1c575176270776783
Chris Brilianto Chandra <rejuvenation@Chriss-MacBook-Pro.local>
2026-03-10 09:18:57 +0800

    Added Input area

--- Question4/Main.java
+++ Question4/Main.java
@@ -1,11 +1,26 @@
 package Question4;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-     
-    }
+         Scanner sc = new Scanner(System.in);
+        System.out.println("Input secret code : ");
+        String inputString = sc.nextLine();  
 
-    public static void main(String[] args) {
+        if (inputString.isEmpty()) {
+            System.out.println("Input is empty");
+        } else {
+            String[] secretCodeInput = inputString.split("\\s+"); 
+            stackingFormationInjection(secretCodeInput);
+
+        }
+        sc.close();
+    }
+     
+
+
+    public static void stackingFormationInjection(String[] secretCodeInput) {
         
     }
 }
