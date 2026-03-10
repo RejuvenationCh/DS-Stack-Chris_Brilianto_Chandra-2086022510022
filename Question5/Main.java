f5f7d76f275b2e373721e9823783c62a00ee1575
Chris Brilianto Chandra <rejuvenation@Chriss-MacBook-Pro.local>
2026-03-10 14:00:52 +0800

    Added functionality for momentum calculation and printing

--- Question5/Main.java
+++ Question5/Main.java
@@ -1,7 +1,30 @@
 package Question5;
 
+import java.util.Scanner;
+import java.util.Stack;
+
 public class Main {
     public static void main(String[] args) {
-     
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input Velocity (n): ");
+        int inputMomentum = sc.nextInt();
+        calculateMomentum(inputMomentum);
+        sc.close();
+
+    }
+    public static void calculateMomentum(Integer inputMomentum) {
+        Stack<Integer> currentNumber = new Stack<>();
+        Integer tempNum = inputMomentum;
+
+        while (tempNum >= 1){
+            currentNumber.push(tempNum);
+            tempNum = tempNum/2;
+        }
+        while (!currentNumber.isEmpty()){
+            System.out.print(currentNumber.pop() + (currentNumber.size() > 0 ? " ": ""));
+        }
+        System.out.println();
+    
+        
     }
 }
