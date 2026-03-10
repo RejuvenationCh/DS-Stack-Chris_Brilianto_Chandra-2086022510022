3d0cdfd01913961cf51589be7d53d8ed4d2c516b
Chris Brilianto Chandra <rejuvenation@Chriss-MacBook-Pro.local>
2026-03-10 16:20:35 +0800

    Added logic and output functionality

--- Question3/Main.java
+++ Question3/Main.java
@@ -22,11 +22,9 @@ public class Main {
         if (tokens.length == 0) return;
         int targetChar = Integer.parseInt(tokens[0]);
         Stack<Long> targetStack = new Stack<>();
-
         for (int i = 1; i <= targetChar && i < tokens.length; i++){
             targetStack.push(Long.parseLong(tokens[i]));
         }
-
         generateInstructions(targetStack);
     }
 
@@ -36,14 +34,35 @@ public class Main {
 
         while (!targets.isEmpty()) {
             long totalValue = targets.pop() + totalSuffixPlus;
-
-
             String instructions = buildInstructions(totalValue);
             outputStack.push(instructions);
-            
-        
             totalSuffixPlus += countEvent(instructions, '+');
         }
 
+        while (!outputStack.isEmpty()) {
+            System.out.print(outputStack.pop());
+        }
+        System.out.println();
+    }
+
+    public static String buildInstructions(long inputTargetValue){
+        if (inputTargetValue == 1) return "1";
+        StringBuilder sb = new StringBuilder("1");
+        String bin = Long.toBinaryString(inputTargetValue);
+        for (int i = 1; i < bin.length(); i++) {
+            sb.append("d+");
+            if (bin.charAt(i) == '1') {
+                sb.append("1+");
+            }
+        }
+        return sb.toString();
+    }
+
+    public static int countEvent(String s, char c){
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == c) count++;
+        }
+        return count;
     }
 }
