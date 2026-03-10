b3d5beb8b7a0dfd198ae5d2c38c5b0af0b3d9290
Chris Brilianto Chandra <rejuvenation@Chriss-MacBook-Pro.local>
2026-03-10 15:02:34 +0800

    Prepping mental for this humongus task

--- Question3/Main.java
+++ Question3/Main.java
@@ -1,8 +1,12 @@
 package Question3;
-
+import java.util.Scanner;
+import java.util.Stack;
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the expression you want : ");
+        String arrayLength = sc.nextLine(); 
+
      
     }
 }
