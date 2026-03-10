0b57a15e803fb150f722e48b71963b1ea9527741
Chris Brilianto Chandra <rejuvenation@Chriss-MacBook-Pro.local>
2026-03-10 12:17:11 +0800

    Made temporary stack

--- Question4/Main.java
+++ Question4/Main.java
@@ -1,10 +1,13 @@
 package Question4;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
-         Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Input secret code : ");
         String inputString = sc.nextLine();  
 
@@ -13,14 +16,28 @@ public class Main {
         } else {
             String[] secretCodeInput = inputString.split("\\s+"); 
             stackingFormationInjection(secretCodeInput);
-
         }
         sc.close();
     }
-     
-
-
+    
     public static void stackingFormationInjection(String[] secretCodeInput) {
+        Stack <String> codeStack = new Stack<>();
+
+        for (String num : secretCodeInput) {
+            codeStack.push(num);
+        }
+        int stackSize = codeStack.size();
+        for (int i = 1; i < stackSize; i++) {
+            List<Integer> tempStack = new ArrayList<>();
+            for (int j = 0; j < i; j++) {
+               if (!codeStack.isEmpty()) {
+                   tempStack.add(Integer.parseInt(codeStack.pop()));
+               }
+            }
+
+            
+        }
         
+
     }
 }
